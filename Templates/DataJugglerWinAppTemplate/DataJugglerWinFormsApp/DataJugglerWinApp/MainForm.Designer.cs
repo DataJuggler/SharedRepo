6ccfb946924919d1e96c254b3a62d4679a2d86ba
Corby <corby@datajuggler.com>
2019-06-03 10:03:46 -0500

    Adding the Shared Repo in its now reduced state.

---
+++ Templates/DataJugglerWinAppTemplate/DataJugglerWinFormsApp/DataJugglerWinApp/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace $safeprojectname$
+{
+    partial class MainForm
+    {
+
+        #region Private Variables
+        private System.Windows.Forms.Panel LeftMarginPanel;
+        private System.Windows.Forms.Panel RightMarginPanel;
+        private System.Windows.Forms.Panel BottomMarginPanel;
+        private System.Windows.Forms.Panel TopMarginPanel;
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        #endregion
+
+        #region Methods
+
+            #region Dispose
+            /// <summary>
+            /// Clean up any resources being used.
+            /// </summary>
+            /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+            #endregion
+
+        #endregion
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.LeftMarginPanel = new System.Windows.Forms.Panel();
+            this.RightMarginPanel = new System.Windows.Forms.Panel();
+            this.BottomMarginPanel = new System.Windows.Forms.Panel();
+            this.TopMarginPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // LeftMarginPanel
+            // 
+            this.LeftMarginPanel.BackColor = System.Drawing.Color.Transparent;
+            this.LeftMarginPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftMarginPanel.Location = new System.Drawing.Point(0, 0);
+            this.LeftMarginPanel.Name = "LeftMarginPanel";
+            this.LeftMarginPanel.Size = new System.Drawing.Size(16, 601);
+            this.LeftMarginPanel.TabIndex = 0;
+            // 
+            // RightMarginPanel
+            // 
+            this.RightMarginPanel.BackColor = System.Drawing.Color.Transparent;
+            this.RightMarginPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightMarginPanel.Location = new System.Drawing.Point(1048, 0);
+            this.RightMarginPanel.Name = "RightMarginPanel";
+            this.RightMarginPanel.Size = new System.Drawing.Size(16, 601);
+            this.RightMarginPanel.TabIndex = 2;
+            // 
+            // BottomMarginPanel
+            // 
+            this.BottomMarginPanel.BackColor = System.Drawing.Color.Transparent;
+            this.BottomMarginPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomMarginPanel.Location = new System.Drawing.Point(16, 585);
+            this.BottomMarginPanel.Name = "BottomMarginPanel";
+            this.BottomMarginPanel.Size = new System.Drawing.Size(1032, 16);
+            this.BottomMarginPanel.TabIndex = 4;
+            // 
+            // TopMarginPanel
+            // 
+            this.TopMarginPanel.BackColor = System.Drawing.Color.Transparent;
+            this.TopMarginPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopMarginPanel.Location = new System.Drawing.Point(16, 0);
+            this.TopMarginPanel.Name = "TopMarginPanel";
+            this.TopMarginPanel.Size = new System.Drawing.Size(1032, 16);
+            this.TopMarginPanel.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::$safeprojectname$.Properties.Resources.Deep_Black;
+            this.ClientSize = new System.Drawing.Size(1064, 601);
+            this.Controls.Add(this.TopMarginPanel);
+            this.Controls.Add(this.BottomMarginPanel);
+            this.Controls.Add(this.RightMarginPanel);
+            this.Controls.Add(this.LeftMarginPanel);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Juggler Win App";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        
+    }
+}
+
---
+++ Templates/DataJugglerWinAppTemplate/DataJugglerWinFormsApp/DataJugglerWinApp/MainForm.cs
@@ -0,0 +1,174 @@
+﻿
+#region using statements
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+#endregion
+
+namespace $safeprojectname$
+{
+
+    #region class MainForm
+    /// <summary>
+    /// This is the main form for this application.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+
+        #region Private Variables
+        private int leftMargin;
+        private int rightMargin;
+        private int topMargin;
+        private int bottomMargin;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Create a new instance of a 'MainForm' object.
+        /// </summary>
+        public MainForm()
+        {
+            // Create Controls
+            InitializeComponent();
+
+            // Perform initializations for this object
+            Init();
+        }
+        #endregion
+
+        #region Events
+
+        #endregion
+
+        #region Methods
+
+        #region Init()
+        /// <summary>
+        /// This method performs initializations for this object.
+        /// </summary>
+        public void Init()
+        {
+            // Set the default value of the margin panels
+            this.LeftMargin = 16;
+            this.TopMargin = 16;
+            this.RightMargin = 16;
+            this.BottomMargin = 16;
+
+            // add any initialization code here
+        }
+        #endregion
+
+        #endregion
+
+        #region Properties
+
+            #region BottomMargin
+            /// <summary>
+            /// This property gets or sets the value for 'BottomMargin'.
+            /// </summary>
+            public int BottomMargin
+            {
+                get { return bottomMargin; }
+                set
+                {
+                    // set the value
+                    bottomMargin = value;
+
+                    // set the value of the control
+                    BottomMarginPanel.Height = bottomMargin;
+                }
+            }
+            #endregion
+
+            #region CreateParams
+            /// <summary>
+            /// This property here is what did the trick to reduce the flickering.
+            /// I also needed to make all of the controls Double Buffered, but
+            /// this was the final touch. It is a little slow when you switch tabs
+            /// but that is because the repainting is finishing before control is
+            /// returned.
+            /// </summary>
+            protected override CreateParams CreateParams
+            {
+                get
+                {
+                    // initial value
+                    CreateParams cp = base.CreateParams;
+
+                    // Apparently this interrupts Paint to finish before showing
+                    cp.ExStyle |= 0x02000000;
+
+                    // return value
+                    return cp;
+                }
+            }
+            #endregion
+
+            #region LeftMargin
+            /// <summary>
+            /// This property gets or sets the value for 'LeftMargin'.
+            /// </summary>
+            public int LeftMargin
+            {
+                get { return leftMargin; }
+                set
+                {
+                    // set the value
+                    leftMargin = value;
+
+                    // set the value of the control
+                    LeftMarginPanel.Width = leftMargin;
+                }
+            }
+            #endregion
+
+            #region RightMargin
+            /// <summary>
+            /// This property gets or sets the value for 'RightMargin'.
+            /// </summary>
+            public int RightMargin
+            {
+                get { return rightMargin; }
+                set
+                {
+                    // set the value
+                    rightMargin = value;
+
+                    // set the width of the control
+                    RightMarginPanel.Width = rightMargin;
+                }
+            }
+            #endregion
+
+            #region TopMargin
+            /// <summary>
+            /// This property gets or sets the value for 'TopMargin'.
+            /// </summary>
+            public int TopMargin
+            {
+                get { return topMargin; }
+                set
+                {
+                    // set the value
+                    topMargin = value;
+
+                    // set the height of the control
+                    TopMarginPanel.Height = topMargin;
+                }
+            }
+            #endregion
+
+        #endregion
+
+    }
+    #endregion
+
+}
